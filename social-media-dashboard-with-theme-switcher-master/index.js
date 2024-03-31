7e0ccd43f45e44cddb956965092d20baf9673fd3
Ioseb Gamilaghdishvili <117769069+Iosalom@users.noreply.github.com>
2024-03-31 19:53:03 +0400

    Delete social-media-dashboard-with-theme-switcher-master directory

--- social-media-dashboard-with-theme-switcher-master/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const body = document.getElementById('body');
-const toggle = document.getElementById('toggle-label-btn');
-
-toggle.addEventListener('click', function () {
-  body.classList.toggle('white-mode');
-});
